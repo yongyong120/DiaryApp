2abe86d99f5440ed23947f80f628a5305fdfb920
JeongahKim <gluck13@naver.com>
2018-10-24 21:15:11 +0900

    Add files via upload
    메모장 기능 구현
    1. 메모장 저장(save icon 버튼, 뒤로가기 버튼)
    수정(short click)
    삭제(long click)
    2. 공유 기능

---
+++ main/java/com/example/gluck/databaseexam_ch20/memoContract.java
@@ -0,0 +1,17 @@
+package com.example.gluck.databaseexam_ch20;
+
+import android.provider.BaseColumns;
+
+// 계약 클래스는 테이블 생성에 대한 sql 문이나 칼럼구조, 테이블명 등을 정의합니다.
+// 스키마 정한 것을 정의
+public final class memoContract {
+    // 인스턴스화 금지
+    private memoContract(){}
+
+    // 테이블 정보를 내부 클래스로 정의
+    public static class MemoEntry implements BaseColumns{
+        public static final String TABLE_NAME = "memo";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_CONTENTS = "contents";
+    }
+}
